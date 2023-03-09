97524e5984c951e227ab10449788377ee00d8ae8
jc271181jc <juanCarlos271181s@gmail.com>
2023-03-09 01:04:28 -0300

    haciendo dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,20 +1,51 @@
 @extends('adminlte::page')
 
-@section('title', 'USUARIOS')
+@section('title', 'DASHBOARD')
 
 @section('content_header')
 <div class="iconUsuario">
-    <img src="./img/add-user.png">
-    <p>Registro</p>
+    <img src="./img/dashboard.png">
+    <p>DASHBOARD</p>
 </div>
-<h1>DASHBOARD EN ELABORACION....</h1>
+
 
 
 @stop
 
 @section('content')
+<div class="row col-6">
+  <canvas id="myChart"></canvas>
+</div>
 
-<img src="./img/freeDashboard.webp" alt="">
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+
+
+
+
 </body>
 
 </html>
@@ -23,10 +54,6 @@
 
 
 <!--div class="row" style="heightx:400px"-->
-
-
-
-
 @stop
 
 @section('css')
@@ -34,7 +61,7 @@
 @stop
 
 @section('js')
-<script>
-    console.log('Hi!');
-</script>
+
+
+
 @stop
